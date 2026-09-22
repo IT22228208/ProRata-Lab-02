00bce8e1eaa72c6791a421256b6c1b7cc74436fd
IT22228208 <It22228208@my.sliit.lk>
2026-09-22 11:10:22 +0530

    Add files via upload

---
+++ Q2/IT22228208Lab2Q2.java
@@ -0,0 +1,9 @@
+public class IT22228208Lab2Q2 {
+    public static void main(String[] args) {
+        int side = 10;
+        int perimeter = 4 * side;
+        double radius = perimeter / (2 * 3.14);
+
+        System.out.println("Radius of the circular fence: " + radius);
+    }
+}
